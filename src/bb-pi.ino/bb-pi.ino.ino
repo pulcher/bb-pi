56be4cd9e284e49f1ee63ae844974256d7a6158f
Harold Pulcher <pulcher@pulcher.biz>
2022-09-16 21:37:45 -0500

    seems the mpu-6050 is doing stuff that is useful.  also adding some example code

--- src/bb-pi.ino/bb-pi.ino.ino
+++ src/bb-pi.ino/bb-pi.ino.ino
@@ -73,64 +73,13 @@ void setup()
   }
   Serial.println("MPU6050 Found!");
 
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
+  //setupt motion detection
+  mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
+  mpu.setMotionDetectionThreshold(1);
+  mpu.setMotionDetectionDuration(1);
+  mpu.setInterruptPinLatch(true);	// Keep it latched.  Will turn off when reinitialized.
+  mpu.setInterruptPinPolarity(true);
+  mpu.setMotionInterrupt(true);
 
   motors.setSpeedA(speedA);
   motors.setSpeedB(speedB);
@@ -162,41 +111,36 @@ void loop()
   // leftServo.write(leftServoPosition);
 
   /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+  // sensors_event_t a, g, temp;
+  // mpu.getEvent(&a, &g, &temp);
 
-  /* Print out the values */
-  // Serial.print("Acceleration X: ");
-  // Serial.print(a.acceleration.x);
-  // Serial.print(", Y: ");
-  // Serial.print(a.acceleration.y);
-  // Serial.print(", Z: ");
-  // Serial.print(a.acceleration.z);
-  // Serial.println(" m/s^2");
+    if(mpu.getMotionInterruptStatus()) {
+      /* Get new sensor events with the readings */
+      sensors_event_t a, g, temp;
+      mpu.getEvent(&a, &g, &temp);
 
-  // Serial.print("Rotation X: ");
-  // Serial.print(g.gyro.x);
-  // Serial.print(", Y: ");
-  // Serial.print(g.gyro.y);
-  // Serial.print(", Z: ");
-  // Serial.print(g.gyro.z);
-  // Serial.println(" rad/s");
-
-  // Serial.print("Temperature: ");
-  // Serial.print(temp.temperature);
-  // Serial.println(" degC");
-
-  // Serial.println("");
+      /* Print out the values */
+      Serial.print("AccelX:");
+      Serial.print(a.acceleration.x);
+      Serial.print(",");
+      Serial.print("AccelY:");
+      Serial.print(a.acceleration.y);
+      Serial.print(",");
+      Serial.print("AccelZ:");
+      Serial.print(a.acceleration.z);
+      Serial.print(", ");
+      Serial.print("GyroX:");
+      Serial.print(g.gyro.x);
+      Serial.print(",");
+      Serial.print("GyroY:");
+      Serial.print(g.gyro.y);
+      Serial.print(",");
+      Serial.print("GyroZ:");
+      Serial.print(g.gyro.z);
+      Serial.println("");
+    }
 
   //delay(10);
-  //Other stuff...
-
-  // digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  // delay(250);                       // wait for a second
-  // digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  // delay(250);                       // wait for a second
-
-  //printInfo();
 }
 
 bool toggle_led(void *) {
